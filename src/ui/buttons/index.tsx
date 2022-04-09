2675295d2ca8bab1ba002cd5e520b25e0e8d64b7
Shiananmiad Bermudez <bermudezdamian7@gmail.com>
2022-04-09 16:49:53 -0300

    modification ui css

--- src/ui/buttons/index.tsx
+++ src/ui/buttons/index.tsx
@@ -1,6 +1,6 @@
 import React from "react";
-import css from "./index.css";
+//import css from "./index.css";
 
 export function MainButton ({children}){
-    return <button className={css.root}>{children}</button>
+    return <button >{children}</button>
 }
--- src/ui/textfield/index.tsx
+++ src/ui/textfield/index.tsx
@@ -1,6 +1,6 @@
 import React from "react";
-import css from "./index.css";
+//import css from "./index.css";
 
 export function TextField (props){
-    return <input {...props}className={css.root}></input>
+    return <input {...props}></input>
 }
