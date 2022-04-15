79335828d581162be706f564d6a4f7c970407a9a
Shiananmiad Bermudez <bermudezdamian7@gmail.com>
2022-04-15 16:42:19 -0300

    cambios en la url de la api

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -1,4 +1,4 @@
-const BASE_URL_API = "https://desafio-final-mod-8.herokuapp.com/";
+const BASE_URL_API = "https://desafio-final-mod-8.herokuapp.com";
 //const BASE_URL_API = "http://localhost:8080";
 
 export async function getPetsAroundMe(geoloc) {
