b273a8dcbd2672aa718e341a94cdc5ae5e6f2b0f
Shiananmiad Bermudez <bermudezdamian7@gmail.com>
2022-04-15 16:25:30 -0300

    cambios en la ui de los componentes

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -1,11 +1,11 @@
 import React from "react";
 import { Outlet } from "react-router-dom";
 import { NavBar } from "../../ui/NavBar/NavBar";
-import css from "./Layout.css";
+
 
 export function Layout() {
   return (
-    <div className={css.root}>
+    <div>
       <header>
         <NavBar />
       </header>
